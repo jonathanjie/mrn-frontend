254eb7d606415643fc3e846870f48706eabf50ca
Eryn Seo <yeryeon@gmail.com>
2022-09-08 18:01:51 +0800

    Create header component

---
+++ src/components/WebHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="bg-white text-black py-4 px-6 md:flex justify-end">
+        <!-- search bar? -->
+        <div class="flex items-center">
+            <span class="text-green-500 text-xl mr-1">
+
+            </span>
+        </div>
+        <ul class="md:flex md:items-center justify-items-end">
+            <li class="md:mx-3">
+                <img src="@/assets/icons/message.svg" class="h-6 w-6"/>
+            </li>
+            <li class="md:mx-3">
+                <img src="@/assets/icons/bell_notification.svg" class="h-6 w-6"/>
+            </li>
+            <li>
+                <img src="@/assets/icons/divider.svg" class="flex h-5 w-6 md:items-center md:visible"/>
+            </li>
+        </ul>
+        <div class="flex md:items-center">
+            <img src="@/assets/icons/avatar.svg" class="h-10 w-10 mx-3"/>
+            <div class="flex flex-col items-left">
+                <span class="font-bold text-gray-900 text-sm mr-1">Mark Hunter Alisson</span>
+                <span class="font-semibold text-gray-500 text-tiny mr-1">Charterers</span>
+            </div>
+            <img src="@/assets/icons/down_arrow.svg" class="h-1.5 w-3 mx-3"/>
+        </div>
+    </div>
+
+</template>
+
+<script>
+</script>
