e1d671721a70f9eb6361d2e6e4e83318a42448ab
Eryn Seo <yeryeon@gmail.com>
2022-09-22 12:36:26 +0800

    Add Gradient Button

---
+++ src/components/GradientButton.vue
@@ -0,0 +1,25 @@
+<template>
+    <button @click="onClick" class="bg-gradient-to-r from-gradientblue to-gradientgreen text-white font-bold text-sm py-2 px-3 rounded inline-flex items-center">
+        <slot name="icon"></slot>
+        <slot name="content">Fallback Content</slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            onClick: {
+                type: Function,
+                required: true
+            }
+        }
+}
+</script>
+
+<style>
+    .gradient {
+        background: linear-gradient(90deg, #0093B8 0%, #09DBA9 100%);
+        border-radius: 8px;
+    }
+
+</style>
