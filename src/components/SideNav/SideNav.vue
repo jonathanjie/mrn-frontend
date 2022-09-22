1c31ebdbe6f552b23103c0aa561099048b86e885
Eryn Seo <yeryeon@gmail.com>
2022-09-22 13:19:26 +0800

    Change SideNav hrefs to router-links

--- src/components/SideNav/SideNav.vue
+++ src/components/SideNav/SideNav.vue
@@ -8,31 +8,33 @@
                 </button>
             </div>
             <div class="flex items-center justify-center py-5 bg-lightblue/[0.24]">
-                <img v-if="collapsed" src="@/assets/logomark_white.svg" class="h-8 px-6"/>
-                <img v-else src="@/assets/marina_logo.svg" class="w-100"/>
+                <router-link to="/">
+                    <img v-if="collapsed" src="@/assets/logomark_white.svg" class="h-8 px-6"/>
+                    <img v-else src="@/assets/marina_logo.svg" class="w-100"/>
+                </router-link>
             </div>
 
             <nav>
                 <div class="py-2"></div>
-                <a href="/" class="flex py-4 px-7 space-x-3 hover:bg-lightblue/[0.24]" :class="{ 'justify-center' : collapsed } ">
+                <router-link to="overview" class="flex py-4 px-7 space-x-3 hover:bg-lightblue/[0.24]" :class="{ 'justify-center' : collapsed } ">
                     <img src="@/assets/icons/overview.svg" class="h-6 w-6"/>
                     <Transition name="fade">
                         <span v-if="!collapsed" class="text-white text-base font-bold">Overview</span>
                     </Transition>
-                </a>
-                <a href="/" class="flex py-4 px-7 space-x-3 hover:bg-lightblue/[0.24]" :class="{ 'justify-center' : collapsed } ">
+                </router-link>
+                <router-link to="my-vessels" class="flex py-4 px-7 space-x-3 hover:bg-lightblue/[0.24]" :class="{ 'justify-center' : collapsed } ">
                     <img src="@/assets/icons/my_vessels.svg" class="h-6 w-6"/>
                     <Transition name="fade">
                         <span v-if="!collapsed" class="text-white text-base font-bold">My Vessels</span>
                     </Transition>
-                </a>
-                <a href="/" class="flex py-4 px-7 space-x-3 hover:bg-lightblue/[0.24]" :class="{ 'justify-center' : collapsed } ">
+                </router-link>
+                <router-link to="plan-voyage" class="flex py-4 px-7 space-x-3 hover:bg-lightblue/[0.24]" :class="{ 'justify-center' : collapsed } ">
                     <img src="@/assets/icons/plan_voyage.svg" class="h-6 w-6"/>
                     <Transition name="fade">
                         <span v-if="!collapsed" class="text-white text-base font-bold">Plan Voyage</span>
                     </Transition>
                     <!-- TODO these should fade out when collapsed -->
-                </a>
+                </router-link>
             </nav>
         </div>
     </div>
