47e9f5a8ca09a8fcb66eb0386cabaf9560f7ab82
Eryn Seo <yeryeon@gmail.com>
2022-09-22 16:27:46 +0800

    Refactor VesselsView for less confusion

---
+++ src/views/MyVesselsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="p-10 space-y-4">
+    <div id="nav" class="px-8 flex flex-col space-y-2">
+      <router-link :to="{ name: 'vessel-overview', params: { vesselname: 'Marina A' , imo: '9876543' } }">Marina A</router-link>
+      <router-link :to="{ name: 'vessel-overview', params: { vesselname: 'Marina B' , imo: '7654321' } }">Marina B</router-link>
+      <router-link :to="{ name: 'vessel-overview', params: { vesselname: 'Marina C' , imo: '1234567' } }">Marina C</router-link>
+    </div>
+  </div>
+</template>
--- src/views/VesselsView.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="vessels">
-    <h1>This is a my vessels page</h1>
-  </div>
-</template>
