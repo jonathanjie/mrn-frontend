d2f08c089b8d1959983fd20ea7f644c89f692c0b
Eryn Seo <yeryeon@gmail.com>
2022-09-22 16:27:25 +0800

    Empty home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,21 +1,7 @@
 <template>
   <div class="flex bg-bggray h-full">
-    <GradientButton>
-      <template v-slot:icon>
-        <img src="@/assets/icons/upload.svg" class="h-5 w-5 mr-2"/>
-      </template>
-      <template v-slot:content>Upload Noon Report</template>  
-    </GradientButton>
   </div>  
 </template>
 
 <script>
-import GradientButton from '../components/GradientButton.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    GradientButton
-}
-}
 </script>
