993c6db2cd4e37b20b5839a8c43c1a08a5a15348
Eryn Seo <yeryeon@gmail.com>
2022-09-22 13:19:34 +0800

    Add necessary routes to index

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import VesselsView from '../views/VesselsView.vue'
+import PlanVoyageView from '../views/PlanVoyageView.vue'
+import OverviewView from '../views/OverviewView.vue'
 
 const routes = [
   {
@@ -8,12 +11,19 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/myvessels',
-    name: 'myvessels',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/VesselsView.vue')
+    path: '/overview',
+    name: 'overview',
+    component: OverviewView
+  },
+  {
+    path: '/my-vessels',
+    name: 'my-vessels',
+    component: VesselsView
+  },
+  {
+    path: '/plan-voyage',
+    name: 'plan-voyage',
+    component: PlanVoyageView
   }
 ]
 
