9109de56530022ba25ace6d69265e1a57250905a
Eryn Seo <yeryeon@gmail.com>
2022-09-22 13:19:51 +0800

    Remove sidenav and header from home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,25 @@
 <template>
-  <div class="flex items-start items-stretch">
-    <div class="z-10">
-      <SideNav/>
-    </div>
-    <div class="grow h-screen">
-      <WebHeader class="sticky"/>
-      <div class="flex bg-bggray h-full"></div>
-    </div>
-  </div>
-  <!-- <GradientButton>
-    <template v-slot:icon>
-      <img src="@/assets/icons/upload.svg" class="h-5 w-5 mr-2"/>
-    </template>
-    <template v-slot:content>Upload Noon Report</template>  
-  </GradientButton> -->
+  <div class="flex bg-bggray h-full">
+    <GradientButton>
+      <template v-slot:icon>
+        <img src="@/assets/icons/upload.svg" class="h-5 w-5 mr-2"/>
+      </template>
+      <template v-slot:content>Upload Noon Report</template>  
+    </GradientButton>
+  </div>  
 </template>
 
 <script>
-import SideNav from '../components/SideNav/SideNav.vue'
-import WebHeader from '../components/WebHeader.vue'
-// import GradientButton from '../components/GradientButton.vue'
+// import SideNav from '../components/SideNav/SideNav.vue'
+// import WebHeader from '../components/WebHeader.vue'
+import GradientButton from '../components/GradientButton.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    SideNav,
-    WebHeader,
-    // GradientButton
+    // SideNav,
+    // WebHeader,
+    GradientButton
 }
 }
 </script>
