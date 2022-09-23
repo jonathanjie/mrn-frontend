d6f9073ef700d43ee4b446bc4bb5ef35dbd262a4
Eryn Seo <yeryeon@gmail.com>
2022-09-23 11:50:42 +0800

    Add custom button component without gradient

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="onClick" class="text-gradientblue bg-superlightblue font-bold text-sm py-2 px-4 h-10 rounded-lg inline-flex items-center border border-gradientblue">
+        <slot name="icon"></slot>
+        <slot name="content">Fallback Content</slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            onClick: {
+                type: Function,
+                required: true
+            }
+        }
+}
+</script>
+
+<style>
+</style>
