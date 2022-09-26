c5fa8339d80d19839fc32754efff2b0a5aeb318c
Eryn Seo <yeryeon@gmail.com>
2022-09-26 19:43:54 +0800

    Modify tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,19 @@ module.exports = {
       teal: colors.teal,
       cyan: colors.cyan,
       gray: colors.gray,
-      blue: '#004557',
+      blue: {
+        DEFAULT: '#004557',
+        800: '#004557',
+        700: '#007694',
+        600: '#0093B8',
+        100: '#CCF5FF',
+        50: '#F0FCFF',
+        system: '#1570EF',
+        sys800: '#1849A9',
+      },
       lightblue: '#007694',
       white: '#fff',
-      bggray: '#f9fafb',
+      darkgray: '#eaeef0',
       gradientblue: '#0093B8',
       gradientgreen: '#09DBA9',
       filtergray: '#1D2939',
@@ -29,7 +38,8 @@ module.exports = {
     fontSize:{
       base: ['14px','24px'],
       tiny: ['12px', '20px'],
-      sm: ['14px', '20px']
+      sm: ['14px', '20px'],
+      huge: ['20px', '30px']
     }
   },
   variants: {
