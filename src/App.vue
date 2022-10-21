900359ec1ecf1db897b7a50e2a2dad1c653fdbe9
Eryn Seo <yeryeon@gmail.com>
2022-10-21 12:17:17 +0800

    Add auth to app

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,15 @@
 <template>
-  <div class="flex items-start items-stretch">
+  <!-- router view for login page
+  <router-view v-if="!isAuthenticated" class="bg-gray-50 min-h-screen"></router-view> -->
+
+  <div v-if="isAuthenticated" class="flex items-start items-stretch">
     <div class="z-50 fixed">
       <SideNav/>
     </div>
     <div class="grow h-screen" :class="collapsed?'ml-20':'ml-64'">
       <!-- TODO: change to fixed, not sticky -->
       <WebHeader class="sticky top-0 z-40"/>
-      <div>
-        <h2>User Profile</h2>
-        <button @click="login">Log in</button>
-        <pre v-if="isAuthenticated">
-            <code>{{ user }}</code>
-            <code>{{ data }}</code>
-            <button @click="logout">Log out</button>
-        </pre>
-      </div>
-      <router-view class="bg-gray-50 min-h-screen"></router-view>
+      <router-view></router-view>
     </div>
   </div>
 </template>
@@ -25,18 +19,16 @@ import SideNav from './components/SideNav/SideNav.vue'
 import WebHeader from './components/WebHeader.vue'
 import { collapsed } from "./components/SideNav/state.js"
 import { useAuth0 } from '@auth0/auth0-vue'
+// import { useRouter } from 'vue-router';
 
 export default {
   setup() {
-    const { loginWithRedirect, user, isAuthenticated, logout, getAccessTokenSilently } = useAuth0();
-
+    const { user, isAuthenticated, logout, getAccessTokenSilently } = useAuth0();
+    
     return {
       collapsed,
       logout: () => {
         logout({ returnTo: window.location.origin });
-      },
-      login: () => {
-        loginWithRedirect();
       },
       doSomethingWithToken: async() => {
         const token = await getAccessTokenSilently();
@@ -45,7 +37,7 @@ export default {
             Authorization: 'Bearer ' + token
           }
         });
-        const data = await response.json();
+        // const data = await response.json();
       },
       user,
       isAuthenticated
