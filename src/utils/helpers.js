550d8204b53537a845cbcb0d473919b6f6dd25a0
Eryn Seo <yeryeon@gmail.com>
2022-11-14 23:01:31 +0800

    create separate helper utility method class

---
+++ src/utils/helpers.js
@@ -0,0 +1,26 @@
+import { ref } from 'vue';
+
+// TODO: handle leading zeros
+const preventNaN = (event, content) => {
+    // only accept 0-9 and one decimal point
+    if (!/\d/.test(event.key) && (event.key !== "." || /\./.test(content))) {
+        return event.preventDefault();  
+    } 
+}
+
+const textInputOptions = ref({
+    format: 'yyyy.MM.dd HH:mm'
+})
+
+const format = (date) => {
+    const day = ('0' + date.getDate()).slice(-2);
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const year = date.getFullYear();
+    const hour = ('0' + date.getHours()).slice(-2);
+    const minute = ('0' + date.getMinutes()).slice(-2);
+
+    return `${year}.${month}.${day} ${hour}:${minute} (LT)`;
+}
+
+
+export default { preventNaN, textInputOptions, format };
