19907a47249dcfcb895f45124f5e1f9dcdaae052
Eryn Seo <yeryeon@gmail.com>
2022-11-17 13:13:58 +0800

    Fix export bug for util functions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,18 +1,18 @@
 import { ref } from 'vue';
 
 // TODO: handle leading zeros
-const preventNaN = (event, content) => {
+export function preventNaN(event, content) {
     // only accept 0-9 and one decimal point
     if (!/\d/.test(event.key) && (event.key !== "." || /\./.test(content))) {
         return event.preventDefault();  
     } 
 }
 
-const textInputOptions = ref({
+export const textInputOptions = ref({
     format: 'yyyy.MM.dd HH:mm'
 })
 
-const format = (date) => {
+export function format(date) {
     const day = ('0' + date.getDate()).slice(-2);
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
     const year = date.getFullYear();
@@ -22,7 +22,7 @@ const format = (date) => {
     return `${year}.${month}.${day} ${hour}:${minute} (LT)`;
 }
 
-const convertDMSToDD = (degrees, minutes, direction) => {
+export function convertDMSToDD(degrees, minutes, direction) {
     var dd = degrees + minutes/60;
 
     if (direction == "S" || direction == "E") {
@@ -31,5 +31,3 @@ const convertDMSToDD = (degrees, minutes, direction) => {
 
     return dd;
 }
-
-export default { preventNaN, textInputOptions, format, convertDMSToDD };
