8ac48acedf1a30e0e6f99e1f7477415cffc9628a
Eryn Seo <yeryeon@gmail.com>
2022-11-17 17:00:34 +0800

    Create Consumption&Condition for departure

--- tailwind.config.js
+++ tailwind.config.js
@@ -91,6 +91,7 @@ module.exports = {
     extend: {
       gridTemplateColumns: {
         '16': 'repeat(16, minmax(0, 1fr))',
+        '30': 'repeat(30, minmax(0, 1fr))',
       },
       boxShadow: {
         'card': '0 2px 2px 2px rgba(29, 41, 57, 0.04)'
