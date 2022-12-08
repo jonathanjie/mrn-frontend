1df0a4610ca4bcb37782a4eb7145385fae54c1c4
Eryn Seo <yeryeon@gmail.com>
2022-12-08 10:40:34 +0900

    Fix custom button size bug

--- src/components/Buttons/CustomButton.vue
+++ src/components/Buttons/CustomButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="onClick" class="text-gradientblue bg-blue-50 font-bold text-14 py-2 px-4 h-10 rounded-lg inline-flex items-center border border-gradientblue">
+    <button @click="onClick" class="text-gradientblue bg-blue-50 font-bold text-14 py-2 px-4 rounded-lg inline-flex items-center border border-gradientblue">
         <slot name="icon"></slot>
         <slot name="content">{{ $t("fallbackContent") }}</slot>
     </button>
