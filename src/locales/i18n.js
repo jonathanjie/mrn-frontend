c005de88dd369a98a844c2705e52a2813d54fb13
Eryn Seo <yeryeon@gmail.com>
2022-12-15 00:15:35 +0900

    Add i18n

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -493,6 +493,7 @@ const i18n = createI18n({
       grounding: "Grounding",
       oilSpill: "Oil spill",
       additionalRemarks: "Additional remarks",
+      other: "Other",
     },
     // Janky way to sort the list
     // 1. console.log the list as a Object, it will be sorted alpabetically by the browser
