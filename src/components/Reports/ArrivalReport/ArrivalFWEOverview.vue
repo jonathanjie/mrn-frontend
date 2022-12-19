7df2d631ee54acac892944f6aeaccf9065963a37
Eryn Seo <yeryeon@gmail.com>
2022-12-19 23:54:13 +0900

    Add time zone and date time to overviews

--- src/components/Reports/ArrivalReport/ArrivalFWEOverview.vue
+++ src/components/Reports/ArrivalReport/ArrivalFWEOverview.vue
@@ -36,17 +36,100 @@
       <div class="hidden xl:block bg-white col-span-2 row-span-1"></div>
       <input class="hidden xl:block bg-white col-span-3 p-3" disabled />
     </div>
+    <div
+      class="col-span-2 xl:col-span-1 grid grid-cols-5 row-span-1 bg-gray-50 text-14 border"
+    >
+      <div class="col-span-2 text-blue-700 p-3 border-r">
+        {{ $t("reportingDateAndTime") }}
+      </div>
+      <DatePicker
+        v-model="reporting_date"
+        class="col-span-3"
+        textInput
+        :textInputOptions="textInputOptions"
+        :format="format"
+        :modelValue="string"
+        :placeholder="$t('selectDateAndTime')"
+      >
+        <template #input-icon>
+          <img src="" />
+        </template>
+      </DatePicker>
+    </div>
+    <div
+      class="col-span-2 xl:col-span-1 grid grid-cols-5 row-span-1 bg-gray-50 text-14 border"
+    >
+      <div class="col-span-2 text-blue-700 p-3 border-r">
+        {{ $t("reportingTimeZone") }}
+      </div>
+      <div class="flex col-span-3 bg-white">
+        <select
+          class="grow self-center p-3 text-14 focus:outline-0"
+          :class="
+            reporting_time_zone === 'default'
+              ? 'text-gray-400'
+              : 'text-gray-700'
+          "
+          v-model="reporting_time_zone"
+        >
+          <option selected disabled value="default">
+            {{ $t("selectTimeZone") }}
+          </option>
+          <option value="-12">UTC-12:00</option>
+          <option value="-11">UTC-11:00</option>
+          <option value="-10">UTC-10:00</option>
+          <option value="-9.5">UTC-9:30</option>
+          <option value="-9">UTC-9:00</option>
+          <option value="-8">UTC-8:00</option>
+          <option value="-7">UTC-7:00</option>
+          <option value="-6">UTC-6:00</option>
+          <option value="-5">UTC-5:00</option>
+          <option value="-4">UTC-4:00</option>
+          <option value="-3.5">UTC-3:30</option>
+          <option value="-3">UTC-3:00</option>
+          <option value="-2">UTC-2:00</option>
+          <option value="-1">UTC-1:00</option>
+          <option value="0">UTC</option>
+          <option value="1">UTC+1:00</option>
+          <option value="2">UTC+2:00</option>
+          <option value="3">UTC+3:00</option>
+          <option value="3.5">UTC+3:30</option>
+          <option value="4">UTC+4:00</option>
+          <option value="4.5">UTC+4:30</option>
+          <option value="5">UTC+5:00</option>
+          <option value="5.5">UTC+5:30</option>
+          <option value="5.75">UTC+5:45</option>
+          <option value="6">UTC+6:00</option>
+          <option value="6.5">UTC+6:30</option>
+          <option value="7">UTC+7:00</option>
+          <option value="8">UTC+8:00</option>
+          <option value="8.75">UTC+8:45</option>
+          <option value="9">UTC+9:00</option>
+          <option value="9.5">UTC+9:30</option>
+          <option value="10">UTC+10:00</option>
+          <option value="10.5">UTC+10:30</option>
+          <option value="11">UTC+11:00</option>
+          <option value="12">UTC+12:00</option>
+          <option value="12.75">UTC+12:45</option>
+          <option value="13">UTC+13:00</option>
+          <option value="14">UTC+14:00</option>
+        </select>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { useArrivalFWEReportStore } from "@/stores/useArrivalFWEReportStore";
 import { storeToRefs } from "pinia";
+import { textInputOptions, format } from "@/utils/helpers";
 
 const store = useArrivalFWEReportStore();
 const {
   reportNo: report_no,
   legNo: leg_no,
   voyageNo: voyage_no,
+  reportingDate: reporting_date,
+  reportingTimeZone: reporting_time_zone,
 } = storeToRefs(store);
 </script>
