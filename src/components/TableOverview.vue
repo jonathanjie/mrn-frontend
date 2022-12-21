0998317f3e72324d8142ff708959c30537290950
Chong Cheng Yang, Lionel <lionel@marinachain.io>
2022-12-21 18:12:13 +0800

    Edited TableOverview

--- src/components/TableOverview.vue
+++ src/components/TableOverview.vue
@@ -9,7 +9,7 @@
       >
         <img
           src="@/assets/icons/Speed_Graph/selected.svg"
-          class="absolute h-5"
+          class="absolute h-5 w-0.5"
         />
         <div
           class="flex rounded-lg justify-between px-3 py-4 items-center"
@@ -23,7 +23,7 @@
           <span
             class="text-16 font-bold"
             :class="selectedSpeed ? 'text-white' : 'text-blue-600'"
-            >{{ speed }}</span
+            >{{ latestData.speed }}</span
           >
         </div>
         <div
@@ -38,7 +38,7 @@
           <span
             class="text-16 font-bold"
             :class="selectedSpeed ? 'text-white' : 'text-blue-500'"
-            >{{ distance }}</span
+            >{{ latestData.distanceToGo }}</span
           >
         </div>
       </button>
@@ -50,21 +50,64 @@
       >
         <img
           src="@/assets/icons/Speed_Graph/selected.svg"
-          class="absolute h-5"
+          class="absolute h-5 w-0.5"
         />
-        <div class="flex rounded-lg justify-between px-3 py-4 items-center">
+        <div
+          class="flex rounded-lg justify-between px-3 py-4 items-center"
+          :class="!selectedSpeed ? 'bg-white bg-opacity-10' : 'bg-gray-100'"
+        >
           <span
             class="text-14"
             :class="!selectedSpeed ? 'text-white' : 'text-gray-700'"
             >{{ $t("foc") }}:</span
           >
-        </div>
-        <div class="flex rounded-lg justify-between px-3 py-4 items-center">
-          <span
-            class="text-14"
-            :class="!selectedSpeed ? 'text-white' : 'text-gray-700'"
-            >{{ $t("rob") }}:</span
+          <div
+            class="grid grid-cols-2 w-full"
+            :class="latestData.fuel.length > 2 ? 'grid-rows-2' : ''"
           >
+            <div v-for="(value, key) in latestData.fuel">
+              <div
+                class="flex rounded-xl bg-gray-100 ml-2 py-1.5 px-2.5 justify-center"
+              >
+                <span
+                  class="text-12"
+                  :class="!selectedSpeed ? 'text-gray-700' : 'text-white'"
+                  >{{ key.toUpperCase() }}:</span
+                >
+                <span
+                  class="text-12 ml-2"
+                  :class="!selectedSpeed ? 'text-blue-600' : 'text-blue-500'"
+                  >{{ value.foc }}</span
+                >
+              </div>
+            </div>
+          </div>
+        </div>
+        <img
+          src="@/assets/icons/Speed_Graph/fuel_divider.svg"
+          class="absolute h-4 justify-self-center"
+        />
+        <div
+          class="grid grid-cols-2 w-full"
+          :class="latestData.fuel.length > 2 ? 'grid-rows-2' : ''"
+        >
+          <div v-for="(value, key) in latestData.fuel">
+            <div
+              class="flex rounded-xl bg-gray-100 ml-2 py-1.5 px-2.5 justify-center"
+              :class="!selectedSpeed ? 'bg-white bg-opacity-10' : 'bg-gray-100'"
+            >
+              <span
+                class="text-12"
+                :class="!selectedSpeed ? 'text-gray-700' : 'text-white'"
+                >{{ key.toUpperCase() }}:</span
+              >
+              <span
+                class="text-12 ml-2"
+                :class="!selectedSpeed ? 'text-blue-600' : 'text-blue-500'"
+                >{{ value.rob }}</span
+              >
+            </div>
+          </div>
         </div>
       </button>
     </div>
@@ -99,7 +142,7 @@ const speed = 14.57;
 const distance = 10.367;
 
 const data = {
-  16_7: {
+  "16_7": {
     speed: 4.0,
     distanceObserved: 0.0,
     distanceToGo: 0.0,
@@ -114,7 +157,7 @@ const data = {
       },
     },
   },
-  17_7: {
+  "17_7": {
     speed: 5.0,
     distanceObserved: 0.0,
     distanceToGo: 0.0,
@@ -129,7 +172,7 @@ const data = {
       },
     },
   },
-  18_7: {
+  "18_7": {
     speed: 6.0,
     distanceObserved: 0.0,
     distanceToGo: 0.0,
@@ -144,7 +187,7 @@ const data = {
       },
     },
   },
-  19_7: {
+  "19_7": {
     speed: 7.0,
     distanceObserved: 0.0,
     distanceToGo: 0.0,
@@ -159,7 +202,7 @@ const data = {
       },
     },
   },
-  20_7: {
+  "20_7": {
     speed: 8.0,
     distanceObserved: 0.0,
     distanceToGo: 0.0,
@@ -174,7 +217,7 @@ const data = {
       },
     },
   },
-  21_7: {
+  "21_7": {
     speed: 9.0,
     distanceObserved: 0.0,
     distanceToGo: 0.0,
@@ -189,7 +232,7 @@ const data = {
       },
     },
   },
-  22_7: {
+  "22_7": {
     speed: 10.0,
     distanceObserved: 0.0,
     distanceToGo: 0.0,
@@ -206,6 +249,8 @@ const data = {
   },
 };
 
+const latestData = data[Object.keys(data).pop()];
+console.log(latestData);
 let selectedSpeed = ref(true);
 const speedList = [];
 const distanceOBSList = [];
@@ -225,6 +270,7 @@ for (let item in data) {
     fuelObject[fuel_type].rob.push(data[item].fuel[fuel_type].rob);
   }
 }
+
 // console.log(speedList);
 // console.log(distanceOBSList);
 // console.log(distanceToGoList);
