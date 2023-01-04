d948be327594c4dc15730e1b4be872d9b8e7af18
XP Liao <mechastriker3@gmail.com>
2023-01-04 16:39:41 +0800

    Add vue query for report details

---
+++ src/views/ReportViews/ReportDetailsView/queries/useReportQuery.js
@@ -0,0 +1,15 @@
+import { useQuery } from "vue-query";
+import axios from "axios";
+
+export const useReportQuery = (uuid) => {
+  return useQuery(["reportDetail"], async () =>
+    axios
+      .get(`https://testapi.marinachain.io/marinanet/reports/${uuid}/`)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error.message);
+      })
+  );
+};
