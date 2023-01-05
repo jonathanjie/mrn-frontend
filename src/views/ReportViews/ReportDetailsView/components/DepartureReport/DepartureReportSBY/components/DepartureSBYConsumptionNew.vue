9eefc5561e2dd41da644591f52c24209c42dbc79
XP Liao <mechastriker3@gmail.com>
2023-01-05 12:59:08 +0800

    Add query

---
+++ src/views/ReportViews/ReportDetailsView/components/DepartureReport/DepartureReportSBY/components/DepartureSBYConsumptionNew.vue
@@ -0,0 +1,503 @@
+<script setup>
+import { preventNaN } from "@/utils/helpers";
+import { ref, computed } from "vue";
+import MiniUnitDisplay from "@/components/MiniUnitDisplay.vue";
+import { useDepartureSBYReportStore } from "@/stores/useDepartureSBYReportStore";
+import { storeToRefs } from "pinia";
+
+const props = defineProps({
+  report: {
+    type: Object,
+    required: true,
+  },
+});
+
+const isFuelOilRemarkEnabled = computed(() =>
+  props.report.consumptionconditiondata.fueloildata_set.reduce(
+    (accum, curr) => accum || curr.fueloildatacorrection,
+    false
+  )
+    ? true
+    : false
+);
+const isLubricatingOilRemarkEnabled = computed(() =>
+  props.report.consumptionconditiondata.lubricatingoildata_set.reduce(
+    (accum, curr) => accum || curr.lubricatingoildatacorrection,
+    false
+  )
+    ? true
+    : false
+);
+
+// const isFuelOilRemarkEnabled = ref(false);
+// const isLubricatingOilRemarkEnabled = ref(false);
+
+// Freshwater Consumption
+const freshwaterConsumed = computed(
+  () => props.report.consumptionconditiondata.freshwaterdata.consumed
+);
+const freshwaterGenerated = computed(
+  () => props.report.consumptionconditiondata.freshwaterdata.generated
+);
+const freshwaterReceiving = computed(
+  () => props.report.consumptionconditiondata.freshwaterdata.received
+);
+const freshwaterDischarging = computed(
+  () => props.report.consumptionconditiondata.freshwaterdata.generated
+);
+const freshwaterChange = computed(
+  () =>
+    +props.report.consumptionconditiondata.freshwaterdata.received +
+    props.report.consumptionconditiondata.freshwaterdata.generated -
+    props.report.consumptionconditiondata.freshwaterdata.discharged -
+    props.report.consumptionconditiondata.freshwaterdata.consumed
+);
+const freshwaterRob = computed(
+  () => props.report.consumptionconditiondata.freshwaterdata.rob
+);
+
+const store = useDepartureSBYReportStore();
+const {
+  fuelOils,
+  lubricatingOils,
+  machinery,
+  // fuel oil
+  fuelOilTotalConsumptions: fuelOilTotalConsumptions,
+  fuelOilRobs: fuelOilRobs,
+  fuelOilBreakdowns: fuelOilBreakdowns,
+  fuelOilDataCorrection: fuelOilDataCorrection,
+  // lubricating oil
+  lubricatingOilBreakdowns: lubricatingOilBreakdowns,
+  lubricatingOilRobs: lubricatingOilRobs,
+  lubricatingOilDataCorrection: lubricatingOilDataCorrection,
+  // fresh water
+//   freshwaterConsumed: freshwaterConsumed,
+//   freshwaterGenerated: freshwaterGenerated,
+//   freshwaterChange: freshwaterChange,
+//   freshwaterReceiving: freshwaterReceiving,
+//   freshwaterDischarging: freshwaterDischarging,
+//   freshwaterRob: freshwaterRob,
+} = storeToRefs(store);
+
+const getFuelOilCols = () => "grid-cols-" + (machinery.value.length + 10);
+</script>
+
+<template>
+  <div class="grid bg-white rounded-lg p-5 gap-4 shadow-card">
+    <div>
+      <div class="flex items-center">
+        <img src="@/assets/icons/selected_blue_gradient.svg" class="h-5 w-5" />
+        <span class="text-16 text-blue-700">
+          <slot>{{ $t("consumptionAndConditionLastReportToSby") }}</slot>
+        </span>
+      </div>
+    </div>
+
+    <div class="grid divide-y divide-dashed gap-8">
+      <div>
+        <div class="self-center mb-4 text-16 text-gray-700 pt-4">
+          {{ $t("fuelOilInMT") }}
+        </div>
+
+        <div class="grid mb-4 text-14" :class="getFuelOilCols()">
+          <div
+            class="col-span-2 border-green-100 bg-green-25 px-6 border-l border-t"
+          ></div>
+          <div
+            v-for="item in machinery"
+            :key="item"
+            class="col-span-1 flex items-center text-blue-700 border-green-100 bg-green-25 p-3 border-t border-l bg-gray-50"
+          >
+            {{ item }}
+          </div>
+          <div
+            class="col-span-2 flex items-center text-blue-700 border-green-100 bg-green-25 p-3 border-t border-l bg-gray-50"
+          >
+            {{ $t("totalConsumption") }}
+          </div>
+          <div
+            class="col-span-2 flex items-center text-blue-700 border-green-100 bg-green-25 p-3 border-t border-l bg-gray-50"
+          >
+            {{ $t("receipt") }}
+          </div>
+          <div
+            class="col-span-2 flex items-center text-blue-700 border-green-100 bg-green-25 p-3 border-t border-l bg-gray-50"
+          >
+            {{ $t("debunkering") }}
+          </div>
+          <div
+            class="col-span-2 flex items-center text-blue-700 border-green-100 bg-green-25 p-3 border-t border-x bg-gray-50"
+          >
+            {{ $t("remainOnBoard") }}
+          </div>
+
+          <span
+            v-for="(fuelOil, index) of fuelOils"
+            :key="index"
+            :class="
+              'col-span-full grid ' +
+              getFuelOilCols() +
+              ' ' +
+              (index == fuelOils.length - 1 ? 'border-b' : '')
+            "
+          >
+            <div
+              class="col-span-2 p-3 text-blue-700 border-t border-l bg-gray-50"
+            >
+              {{ fuelOil }}
+            </div>
+            <input
+              v-for="item of machinery"
+              :key="item"
+              v-model="fuelOilBreakdowns[fuelOil][item]"
+              @keypress="preventNaN($event, fuelOilBreakdowns[fuelOil][item])"
+              placeholder="0"
+              class="col-span-1 p-3 pl-4 border-t border-l bg-white text-gray-700 focus:outline-0"
+            />
+            <div
+              class="col-span-2 text-gray-400 p-3 border-t border-l bg-gray-25"
+            >
+              {{ fuelOilTotalConsumptions[fuelOil] }}
+            </div>
+            <input
+              v-model="fuelOilBreakdowns[fuelOil].receipt"
+              @keypress="
+                preventNaN($event, fuelOilBreakdowns[fuelOil].receipt)
+              "
+              placeholder="0"
+              class="col-span-2 p-3 pl-4 border-t border-l bg-white text-gray-700 focus:outline-0"
+            />
+            <input
+              v-model="fuelOilBreakdowns[fuelOil].debunkering"
+              @keypress="
+                preventNaN($event, fuelOilBreakdowns[fuelOil].debunkering)
+              "
+              placeholder="0"
+              class="col-span-2 p-3 pl-4 border-t border-l bg-white text-gray-700 focus:outline-0"
+            />
+            <div
+              class="col-span-2 text-gray-400 p-3 border-t border-x bg-gray-25"
+            >
+              {{ fuelOilRobs[fuelOil] }}
+            </div>
+          </span>
+        </div>
+
+        <div
+          v-if="!isFuelOilRemarkEnabled"
+          class="bg-gray-25 flex items-center py-4 px-3 border border-gray-100 cursor-pointer"
+          @click="isFuelOilRemarkEnabled = !isFuelOilRemarkEnabled"
+        >
+          <img
+            src="@/assets/icons/checkboxes/unchecked_square.svg"
+            class="mr-2 h-5 w-5"
+          />
+          <span class="text-gray-700">{{ $t("additionalRemarks") }}</span>
+        </div>
+        <div
+          v-else
+          class="bg-gray-25 flex-col py-4 px-3 border border-gray-100"
+        >
+          <div
+            class="flex items-center mb-3 cursor-pointer"
+            @click="isFuelOilRemarkEnabled = !isFuelOilRemarkEnabled"
+          >
+            <img
+              src="@/assets/icons/checkboxes/checked_square.svg"
+              class="mr-2 h-5 w-5"
+            />
+            <span class="text-gray-700">{{ $t("additionalRemarks") }}</span>
+          </div>
+          <div class="grid grid-cols-10 border text-14">
+            <div class="col-span-2 text-blue-700 p-3 bg-gray-50">
+              {{ $t("correction") }}
+            </div>
+            <select
+              v-model="fuelOilDataCorrection.type"
+              class="col-span-4 p-3 border-l focus:outline-0"
+              :class="
+                fuelOilDataCorrection.type === 'default'
+                  ? 'text-gray-400'
+                  : 'text-gray-700'
+              "
+            >
+              <option selected disabled value="default">
+                {{ $t("selectType") }}
+              </option>
+              <option
+                v-for="fuelOil of fuelOils"
+                :value="fuelOil"
+                :key="fuelOil"
+              >
+                {{ fuelOil }}
+              </option>
+            </select>
+            <div class="flex col-span-4 p-3 pl-4 border-l bg-white">
+              <input
+                v-model="fuelOilDataCorrection.correction"
+                @keypress="
+                  preventNaN($event, fuelOilDataCorrection.correction)
+                "
+                placeholder="00,000.00"
+                class="w-24 text-gray-700 focus:outline-0"
+              />
+              <MiniUnitDisplay>MT</MiniUnitDisplay>
+            </div>
+            <div
+              class="col-span-2 row-span-2 text-blue-700 p-3 border-t bg-gray-50"
+            >
+              {{ $t("remarks") }}
+            </div>
+            <textarea
+              v-model.trim="fuelOilDataCorrection.remarks"
+              placeholder="Input description here"
+              class="col-span-8 row-span-2 border-t border-l p-3 pl-4 bg-white text-gray-700 focus:outline-0"
+            ></textarea>
+          </div>
+        </div>
+      </div>
+
+      <div class="pt-8">
+        <div class="self-center text-16 mb-4 text-gray-700">
+          {{ $t("lubricatingOilInL") }}
+        </div>
+
+        <div class="grid grid-cols-10 text-14 mb-4">
+          <div
+            class="col-span-2 border-yellow-100 bg-yellow-25 px-6 border-l border-t"
+          ></div>
+          <div
+            class="col-span-2 flex items-center text-yellow-800 border-yellow-100 bg-yellow-25 p-3 border-t border-l bg-gray-50"
+          >
+            {{ $t("consumption") }}
+          </div>
+          <div
+            class="col-span-2 flex items-center text-yellow-800 border-yellow-100 bg-yellow-25 p-3 border-t border-l bg-gray-50"
+          >
+            {{ $t("receipt") }}
+          </div>
+          <div
+            class="col-span-2 flex items-center text-yellow-800 border-yellow-100 bg-yellow-25 p-3 border-t border-l bg-gray-50"
+          >
+            {{ $t("debunkering") }}
+          </div>
+          <div
+            class="col-span-2 flex items-center text-yellow-800 border-yellow-100 bg-yellow-25 p-3 border-t border-x bg-gray-50"
+          >
+            {{ $t("remainOnBoard") }}
+          </div>
+
+          <span
+            v-for="(lubricatingOil, index) of lubricatingOils"
+            :key="index"
+            :class="
+              'col-span-10 grid grid-cols-10' +
+              (index == lubricatingOils.length - 1 ? ' border-b' : '')
+            "
+          >
+            <div
+              class="col-span-2 p-3 text-blue-700 border-t border-l bg-gray-50"
+            >
+              {{ lubricatingOil }}
+            </div>
+            <input
+              v-model="
+                lubricatingOilBreakdowns[lubricatingOil]['total_consumption']
+              "
+              @keypress="
+                preventNaN(
+                  $event,
+                  lubricatingOilBreakdowns[lubricatingOil][
+                    'total_consumption'
+                  ]
+                )
+              "
+              placeholder="0"
+              class="col-span-2 p-3 pl-4 border-t border-l bg-white text-gray-700 focus:outline-0"
+            />
+            <input
+              v-model="lubricatingOilBreakdowns[lubricatingOil]['receipt']"
+              @keypress="
+                preventNaN(
+                  $event,
+                  lubricatingOilBreakdowns[lubricatingOil]['receipt']
+                )
+              "
+              placeholder="0"
+              class="col-span-2 p-3 pl-4 border-t border-l bg-white text-gray-700 focus:outline-0"
+            />
+            <input
+              v-model="
+                lubricatingOilBreakdowns[lubricatingOil]['debunkering']
+              "
+              @keypress="
+                preventNaN(
+                  $event,
+                  lubricatingOilBreakdowns[lubricatingOil]['debunkering']
+                )
+              "
+              placeholder="0"
+              class="col-span-2 p-3 pl-4 border-t border-l bg-white text-gray-700 focus:outline-0"
+            />
+            <div
+              class="col-span-2 text-gray-400 p-3 border-t border-x bg-gray-25"
+            >
+              {{ lubricatingOilRobs[lubricatingOil] }}
+            </div>
+          </span>
+        </div>
+
+        <div
+          v-if="!isLubricatingOilRemarkEnabled"
+          class="bg-gray-25 flex items-center py-4 px-3 border border-gray-100 cursor-pointer"
+          @click="
+            isLubricatingOilRemarkEnabled = !isLubricatingOilRemarkEnabled
+          "
+        >
+          <img
+            src="@/assets/icons/checkboxes/unchecked_square.svg"
+            class="mr-2 h-5 w-5"
+          />
+          <span class="text-gray-700">{{ $t("additionalRemarks") }}</span>
+        </div>
+        <div
+          v-else
+          class="bg-gray-25 flex-col py-4 px-3 border border-gray-100"
+        >
+          <div
+            class="flex items-center mb-3 cursor-pointer"
+            @click="
+              isLubricatingOilRemarkEnabled = !isLubricatingOilRemarkEnabled
+            "
+          >
+            <img
+              src="@/assets/icons/checkboxes/checked_square.svg"
+              class="mr-2 h-5 w-5"
+            />
+            <span class="text-gray-700">{{ $t("additionalRemarks") }}</span>
+          </div>
+          <div class="grid grid-cols-14 border text-14">
+            <div class="col-span-2 text-blue-700 p-3 bg-gray-50">
+              {{ $t("correction") }}
+            </div>
+            <select
+              v-model="lubricatingOilDataCorrection.type"
+              class="col-span-6 p-3 border-l focus:outline-0"
+              :class="
+                lubricatingOilDataCorrection.type === 'default'
+                  ? 'text-gray-400'
+                  : 'text-gray-700'
+              "
+            >
+              <option selected disabled value="default">
+                {{ $t("selectType") }}
+              </option>
+              <option
+                v-for="lubricatingOil of lubricatingOils"
+                :value="lubricatingOil"
+                :key="lubricatingOil"
+              >
+                {{ lubricatingOil }}
+              </option>
+            </select>
+            <div class="flex col-span-6 p-3 pl-4 border-l bg-white">
+              <input
+                v-model="lubricatingOilDataCorrection.correction"
+                @keypress="
+                  preventNaN($event, lubricatingOilDataCorrection.correction)
+                "
+                placeholder="00,000.00"
+                class="w-24 text-gray-700 focus:outline-0"
+              />
+              <MiniUnitDisplay>MT</MiniUnitDisplay>
+            </div>
+            <div
+              class="col-span-2 row-span-2 text-blue-700 p-3 border-t bg-gray-50"
+            >
+              {{ $t("remarks") }}
+            </div>
+            <textarea
+              v-model.trim="lubricatingOilDataCorrection.remarks"
+              :placeholder="$t('inputDescriptionHere')"
+              class="col-span-12 row-span-2 border-t border-l p-3 pl-4 bg-white text-gray-700 focus:outline-0"
+            ></textarea>
+          </div>
+        </div>
+      </div>
+
+      <div class="pt-8">
+        <div class="self-center text-16 text-gray-700">
+          {{ $t("freshWaterInTon") }}
+        </div>
+        <div class="grid grid-cols-6 pt-8 text-14">
+          <div
+            class="col-span-1 text-sysblue-800 p-3 border-t border-l border-sysblue-100 bg-sysblue-25"
+          >
+            {{ $t("consumed") }}
+          </div>
+          <div
+            class="col-span-1 text-sysblue-800 p-3 border-t border-l border-sysblue-100 bg-sysblue-25"
+          >
+            {{ $t("generated") }}
+          </div>
+          <div
+            class="col-span-1 text-sysblue-800 p-3 border-t border-l border-sysblue-100 bg-sysblue-25"
+          >
+            +/-
+          </div>
+          <div
+            class="col-span-1 text-sysblue-800 p-3 border-t border-l border-sysblue-100 bg-sysblue-25"
+          >
+            {{ $t("receipt") }}
+          </div>
+          <div
+            class="col-span-1 text-sysblue-800 p-3 border-t border-l border-sysblue-100 bg-sysblue-25"
+          >
+            {{ $t("discharging") }}
+          </div>
+          <div
+            class="col-span-1 text-sysblue-800 p-3 border-t border-x border-sysblue-100 bg-sysblue-25"
+          >
+            {{ $t("remainOnBoard") }}
+          </div>
+          <input
+            v-model="freshwaterConsumed"
+            @keypress="preventNaN($event, freshwaterConsumed)"
+            placeholder="0"
+            class="col-span-1 p-3 pl-4 border-y border-l bg-white text-gray-700 focus:outline-0"
+          />
+          <input
+            v-model="freshwaterGenerated"
+            @keypress="preventNaN($event, freshwaterGenerated)"
+            placeholder="0"
+            class="col-span-1 p-3 pl-4 border-y border-l bg-white text-gray-700 focus:outline-0"
+          />
+          <div
+            class="col-span-1 text-gray-400 p-3 border-y border-l bg-gray-25"
+          >
+            {{ freshwaterChange }}
+          </div>
+          <input
+            v-model="freshwaterReceiving"
+            @keypress="preventNaN($event, freshwaterReceiving)"
+            placeholder="0"
+            class="col-span-1 p-3 pl-4 border-y border-l bg-white text-gray-700 focus:outline-0"
+          />
+          <input
+            v-model="freshwaterDischarging"
+            @keypress="preventNaN($event, freshwaterDischarging)"
+            placeholder="0"
+            class="col-span-1 p-3 pl-4 border-y border-l bg-white text-gray-700 focus:outline-0"
+          />
+          <div
+            class="col-span-1 text-gray-400 p-3 border-y border-x bg-gray-25"
+          >
+            {{ freshwaterRob }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
