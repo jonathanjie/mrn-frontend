4bc877ab6cabb4ed1a8ee2fd713900662d6bfde9
Eryn Seo <yeryeon@gmail.com>
2023-01-09 00:54:15 +0800

    Add i18n

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -522,6 +522,8 @@ const i18n = createI18n({
       reportType: "Report Type",
       reports: "Reports",
       leaveFeedback: "Leave Feedback",
+      manager: "Manager",
+      crew: "Crew",
     },
     // Janky way to sort the list
     // 1. console.log the list as a Object, it will be sorted alpabetically by the browser
