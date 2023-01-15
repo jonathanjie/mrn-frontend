1b134eb2c70c30e719b3e6408cae276ddd4ae366
Chong Cheng Yang, Lionel <lionel@marinachain.io>
2023-01-15 22:21:35 +0800

    Added some changes to the Empty Page

--- src/views/EmptyPage.vue
+++ src/views/EmptyPage.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="flex flex-col p-24 pb-52 justify-center items-center w-full">
     <img src="@/assets/icons/empty.svg" class="h-28 w-28" />
-    <div class="flex">
+    <div class="flex align-items-center">
       <span class="text-lg font-bold text-gray-800 pt-3"
         >{{ $t("clickMyVessels") }}-></span
       >
-      <img
-        src="@/assets/icons/my_vessels.svg"
-        class="h-10 w-10 bg-blue rounded-xl"
-      />
+      <div class="flex rounded-xl bg-blue items-center">
+        <img src="@/assets/icons/my_vessels.svg" class="h-6 w-6" />
+        <span class="text-white text-14 font-bold my-2 mr-2">{{
+          $t("myVessels")
+        }}</span>
+      </div>
     </div>
   </div>
 </template>
