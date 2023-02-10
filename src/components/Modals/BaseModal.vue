8d9a8126cc967bd928cd2028ff4e7be761e92527
Eryn Seo <eryn@marinachain.io>
2023-02-10 17:23:07 +0800

    Edit Base modal

--- src/components/Modals/BaseModal.vue
+++ src/components/Modals/BaseModal.vue
@@ -28,36 +28,3 @@ const props = defineProps({
     </div>
   </div>
 </template>
-
-<style>
-/* .modal-body {
-  margin: 20px 0;
-}
-
-.modal-default-button {
-  float: right;
-} */
-
-/*
- * The following styles are auto-applied to elements with
- * transition="modal" when their visibility is toggled
- * by Vue.js.
- *
- * You can easily play with the modal transition by editing
- * these styles.
- */
-
-/* .modal-enter-from {
-  opacity: 0;
-}
-
-.modal-leave-to {
-  opacity: 0;
-}
-
-.modal-enter-from .modal-container,
-.modal-leave-to .modal-container {
-  -webkit-transform: scale(1.1);
-  transform: scale(1.1);
-} */
-</style>
