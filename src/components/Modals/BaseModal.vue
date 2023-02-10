efd06370f797e37d712af58a1cf13dd7a9f9757e
Eryn Seo <eryn@marinachain.io>
2023-02-10 17:23:02 +0800

    Edit base modal

--- src/components/Modals/BaseModal.vue
+++ src/components/Modals/BaseModal.vue
@@ -5,30 +5,28 @@ const props = defineProps({
 </script>
 
 <template>
-  <!-- <Transition name="modal"> -->
   <div
     v-show="props.show"
     class="fixed z-50 top-0 left-0 w-full h-full bg-black/50 flex"
   >
-    <div
-      class="w-1/2 m-auto pt-5 px-7 bg-white border border-2 drop-shadow rounded-xl"
-    >
-      <div class="mb-5">
-        <button class="float-right" @click="$emit('close')">X</button>
+    <div class="w-1/2 m-auto bg-white border border-2 drop-shadow rounded-xl">
+      <div class="py-5 px-7 border-b">
+        <button class="float-right text-gray-800" @click="$emit('close')">
+          X
+        </button>
 
         <slot name="header">default header</slot>
       </div>
 
-      <div class="">
+      <div class="max-h-[40rem] overflow-auto">
         <slot name="body">default body</slot>
       </div>
 
-      <div class="bg-gray-50">
-        <slot name="footer"> default footer </slot>
+      <div class="bg-gray-50 px-7 py-4 rounded-b-xl">
+        <slot name="footer">default footer</slot>
       </div>
     </div>
   </div>
-  <!-- </Transition> -->
 </template>
 
 <style>
