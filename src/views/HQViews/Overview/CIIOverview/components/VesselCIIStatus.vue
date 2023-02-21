b3ae5e789fe3445954f393cb85a1403746fb4167
Chong Cheng Yang, Lionel <lionel@marinachain.io>
2023-02-21 11:47:20 +0800

    Added live value portion

--- src/views/HQViews/Overview/CIIOverview/components/VesselCIIStatus.vue
+++ src/views/HQViews/Overview/CIIOverview/components/VesselCIIStatus.vue
@@ -1,9 +1,18 @@
 <script setup>
 import CustomButton from "@/components/Buttons/CustomButton.vue";
 const currentYear = 2023;
+const currentStats = {
+  year: 2023,
+  grade: "A",
+  gradeValue: 4.62,
+  ytd: "2023-02-10",
+  targetCIIGrade: "A",
+  targetCIIGradeValue: 4.62,
+  cLevelLimit: 5.0,
+};
 </script>
 <template>
-  <div class="flex flex col">
+  <div class="flex flex-col">
     <div class="flex justify-between items-center">
       <span class="text-18 text-gray-700 font-bold">{{ $t("ciiStatus") }}</span>
       <CustomButton
@@ -17,11 +26,23 @@ const currentYear = 2023;
         >
       </CustomButton>
     </div>
-    <div class="flex space-x-6 w-full">
-      <div class="flex flex-col rounded-l bg-white p-4">
-        <span class="text-14 text-blue-700 font-semibold mr-1.5">{{
-          currentYear
-        }}</span>
+    <div class="flex space-x-6">
+      <div class="flex flex-col rounded-xl bg-white p-4">
+        <div class="flex flex-row">
+          <span class="text-14 text-blue-700 font-semibold mr-1.5">{{
+            currentYear
+          }}</span>
+          <div class="flex rounded-xl h-7 w-auto bg-red-50 ml-2">
+            <ul class="list-disc p-0.5 text-14 ml-6 mr-3">
+              <li class="text-red-500 font-bold">
+                <span class="text-red-700 font-bold">{{
+                  $t("currentStats")
+                }}</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <span class="w-full"></span>
       </div>
     </div>
   </div>
