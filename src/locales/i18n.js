c0cafbcfa2c31b3ae62909599048e8bbf51645ee
Chong Cheng Yang, Lionel <chonglionel97@gmail.com>
2023-03-02 23:50:46 +0800

    Added fallback content for empty legs

--- src/locales/i18n.js
+++ src/locales/i18n.js
@@ -568,6 +568,9 @@ const i18n = createI18n({
       createNewLeg: "Create New Leg",
       contactUs: "Contact Us",
       popular: "POPULAR",
+      noLegCreated: "No Leg Created",
+      clickOnCreateNewLegAboveToBegin:
+        "Click on 'Create New Leg' above to begin",
     },
     // Janky way to sort the list
     // 1. console.log the list as a Object, it will be sorted alpabetically by the browser
